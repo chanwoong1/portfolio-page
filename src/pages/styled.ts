d503e13373ae293a40d98eb24e54bb084e961bbb
Chanwoong1 <jcw7468@gmail.com>
2023-10-10 22:49:20 +0900

    Chore: 초기 세팅

---
+++ src/pages/styled.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+export const FirstBackground = styled.div`
+  background-color: #f5f5f5;
+  width: 100%;
+  border: 1px solid #000;
+`;
+
+export const SecondBackground = styled.div`
+  background-color: #e0e0e0;
+  width: 100%;
+  border: 1px solid #000;
+`;

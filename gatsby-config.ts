757344f6bdc8c014de12b531ed01929e9fd2d5c3
Chanwoong1 <jcw7468@gmail.com>
2023-10-11 00:35:50 +0900

    Chore: 메타데이터 추가

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,8 @@
 import type { GatsbyConfig } from "gatsby";
+import siteMetaData from "./gatsby-site-meta-data"
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `chanwoong's portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
+  siteMetadata: siteMetaData,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen

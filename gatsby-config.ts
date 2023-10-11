2e69ca6198c65c464d589fdb9b533d78a644267a
Chanwoong1 <jcw7468@gmail.com>
2023-10-11 16:04:07 +0900

    Chore: favicon 추가

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,14 +7,30 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-mdx",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     },
-    __key: "pages"
-  }]
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `chanwoong's portfolio`,
+        short_name: `chanwoong's portfolio`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `./static/favicon.png`
+      },
+    },
+  ]
 };
 
 export default config;

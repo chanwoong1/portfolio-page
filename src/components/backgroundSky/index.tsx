3d62cd698e0027424855b619bad9524eba53c6b7
Chanwoong1 <jcw7468@gmail.com>
2023-10-13 00:32:00 +0900

    Refactor: apply memoization

--- src/components/backgroundSky/index.tsx
+++ src/components/backgroundSky/index.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { memo, useMemo } from "react";
 import * as S from "./index.styled";
 
 const BackgroundSky: React.FC = () => {
-  const numberOfStars = Math.floor(Math.random() * 50) + 50;
-  const stars = new Array(numberOfStars).fill(true);
+  const numberOfStars = useMemo(() => Math.floor(Math.random() * 50) + 50, []);
+  const stars = useMemo(
+    () => new Array(numberOfStars).fill(true),
+    [numberOfStars]
+  );
+
   return (
     <S.BackgroundWrapper>
       {stars.map((el, idx) => {
-        const sizeOfStar = Math.floor(Math.random() * 3) + 1;
+        const sizeOfStar = useMemo(() => Math.floor(Math.random() * 3) + 1, []);
         return (
           <S.Star
             key={"star" + idx}
             style={{
-              left: Math.random() * 100 + "vw",
-              top: Math.random() * 400 + "vh",
-              width: sizeOfStar + "px",
-              height: sizeOfStar + "px",
+              left: useMemo(() => Math.random() * 100 + "vw", []),
+              top: useMemo(() => Math.random() * 400 + "vh", []),
+              width: useMemo(() => sizeOfStar + "px", [sizeOfStar]),
+              height: useMemo(() => sizeOfStar + "px", [sizeOfStar]),
             }}
           />
         );
@@ -24,4 +28,4 @@ const BackgroundSky: React.FC = () => {
   );
 };
 
-export default BackgroundSky;
+export default memo(BackgroundSky);

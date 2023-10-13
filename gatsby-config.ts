d5edd2388ff381d033f19a0a847e50fe345c92ef
chanwoong1 <jcw7468@gmail.com>
2023-10-13 17:23:06 +0900

    Chore: add script

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,6 +3,7 @@ import siteMetaData from "./gatsby-site-meta-data";
 
 const config: GatsbyConfig = {
   siteMetadata: siteMetaData,
+  pathPrefix: "/portfolio-page",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen

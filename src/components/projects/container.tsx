cc1b5ed2ea1e564aafbed4a4a1f9e48e480149d7
Chanwoong1 <jcw7468@gmail.com>
2023-10-13 11:58:10 +0900

    Refactor: code splitting

---
+++ src/components/projects/container.tsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useRef } from "react";
+import { Network } from "vis-network";
+import projects from "../../data/project";
+import star from "../../images/star_white.svg";
+
+interface ProjectsProps {
+  refArray: React.RefObject<HTMLDivElement>[];
+  innerWidth: number;
+  innerHeight: number;
+  setSelectedProject: React.Dispatch<React.SetStateAction<ProjectType | null>>;
+}
+
+export type ProjectType = {
+  title: string;
+  description: string;
+  image: string;
+  x: number;
+  y: number;
+};
+
+export const ProjectsOf42Seoul: React.FC<ProjectsProps> = ({
+  refArray,
+  innerWidth,
+  innerHeight,
+  setSelectedProject,
+}: ProjectsProps) => {
+  const visGraphRef = useRef<HTMLDivElement | null>(null);
+
+  const nodes = projects.map((project, idx) => ({
+    id: idx,
+    label: project.title,
+    x: innerWidth < 350 ? innerWidth * project.x : 350 * project.x,
+    y: innerHeight < 600 ? innerHeight * project.y : 600 * project.y,
+  }));
+
+  const edges = [
+    { from: 0, to: 1 },
+    { from: 1, to: 2 },
+    { from: 2, to: 3 },
+    { from: 3, to: 4 },
+    { from: 4, to: 5 },
+    { from: 5, to: 6 },
+  ];
+
+  useEffect(() => {
+    if (visGraphRef.current) {
+      const network = new Network(
+        visGraphRef.current,
+        {
+          nodes,
+          edges,
+        },
+        {
+          autoResize: true,
+          nodes: {
+            shape: "image",
+            image: star,
+            size: 15,
+            font: {
+              size: 15,
+              color: "white",
+            },
+            borderWidth: 1,
+            borderWidthSelected: 3,
+            shadow: true,
+            color: "white",
+            fixed: {
+              x: true,
+              y: true,
+            },
+            chosen: {
+              node: false,
+              label: (values, id, selected, hovering) => {
+                values.size = 20;
+              },
+            },
+          },
+          edges: {
+            width: 1,
+            color: "rgba(255, 255, 255,  0.5)",
+            shadow: true,
+            smooth: false,
+            chosen: false,
+          },
+          interaction: {
+            dragNodes: false,
+            dragView: false,
+            zoomView: false,
+            hover: true,
+          },
+        }
+      );
+
+      network?.on("click", (event) => {
+        const projectNo = event.nodes[0];
+        setSelectedProject(projects[projectNo]);
+      });
+    }
+  }, [visGraphRef, nodes, edges, refArray]);
+
+  return (
+    <div
+      ref={visGraphRef}
+      style={{
+        height: `${innerHeight < 600 ? innerHeight : 600}px`,
+        width: `${innerWidth < 350 ? innerWidth : 350}px`,
+      }}
+    />
+  );
+};
--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import * as S from "./index.styled";
-import { Network } from "vis-network";
-import projects from "../../data/project";
-import star from "../../images/star_white.svg";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import { ProjectType, ProjectsOf42Seoul } from "./container";
 
 interface ProjectsProps {
   projectsRef: React.RefObject<HTMLDivElement>;
@@ -14,109 +12,30 @@ interface ProjectsProps {
   innerHeight: number;
 }
 
-type ProjectType = {
-  title: string;
-  description: string;
-  image: string;
-  x: number;
-  y: number;
-};
-
 const Projects: React.FC<ProjectsProps> = ({
   projectsRef,
   refArray,
   innerWidth,
   innerHeight,
 }: ProjectsProps) => {
-  const visGraphRef = useRef<HTMLDivElement | null>(null);
   const [selectedProject, setSelectedProject] =
     React.useState<ProjectType | null>(null);
-  console.log(innerHeight, innerWidth);
-
-  const nodes = projects.map((project, idx) => ({
-    id: idx,
-    label: project.title,
-    x: innerWidth < 350 ? innerWidth * project.x : 350 * project.x,
-    y: innerHeight < 600 ? innerHeight * project.y : 600 * project.y,
-  }));
-
-  const edges = [
-    { from: 0, to: 1 },
-    { from: 1, to: 2 },
-    { from: 2, to: 3 },
-    { from: 3, to: 4 },
-    { from: 4, to: 5 },
-    { from: 5, to: 6 },
-  ];
 
   useEffect(() => {
-    if (visGraphRef.current) {
-      const network = new Network(
-        visGraphRef.current,
-        {
-          nodes,
-          edges,
-        },
-        {
-          autoResize: true,
-          nodes: {
-            shape: "image",
-            image: star,
-            size: 15,
-            font: {
-              size: 15,
-              color: "white",
-            },
-            borderWidth: 1,
-            borderWidthSelected: 3,
-            shadow: true,
-            color: "white",
-            fixed: {
-              x: true,
-              y: true,
-            },
-            chosen: {
-              node: false,
-              label: (values, id, selected, hovering) => {
-                values.size = 20;
-              },
-            },
-          },
-          edges: {
-            width: 1,
-            color: "rgba(255, 255, 255,  0.5)",
-            shadow: true,
-            smooth: false,
-            chosen: false,
-          },
-          interaction: {
-            dragNodes: false,
-            dragView: false,
-            zoomView: false,
-            hover: true,
-          },
-        }
-      );
-
-      network?.on("click", (event) => {
-        const projectNo = event.nodes[0];
-        setSelectedProject(projects[projectNo]);
-      });
-    }
-  }, [visGraphRef, nodes, edges, refArray]);
+    console.log(selectedProject);
+  }, [selectedProject]);
 
   return (
     <S.ProjectWrapper id="projects" ref={projectsRef}>
       <S.ProjectTitle>PROJECTS</S.ProjectTitle>
-      <div
-        ref={visGraphRef}
-        style={{
-          height: `${innerHeight < 600 ? innerHeight : 600}px`,
-          width: `${innerWidth < 350 ? innerWidth : 350}px`,
-        }}
+      <ProjectsOf42Seoul
+        refArray={refArray}
+        innerHeight={innerHeight}
+        innerWidth={innerWidth}
+        setSelectedProject={setSelectedProject}
       />
       <Modal
-        open={selectedProject !== null}
+        open={selectedProject instanceof Object}
         onClose={() => setSelectedProject(null)}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"

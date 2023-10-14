71320c28433698a587bf7f59d0952fe5423fc376
chanwoong1 <jcw7468@gmail.com>
2023-10-14 11:41:06 +0900

    Refactor: apply useEffect with Error#418

--- src/components/tistory/index.tsx
+++ src/components/tistory/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./index.styled";
 import RecentlyPosts from "./container";
 import Box from "@mui/material/Box";
@@ -26,96 +26,98 @@ const Blog: React.FC<BlogProps> = ({
   const [blogRecentlyPosts, setBlogRecentlyPosts] = useState<any[]>([]);
   const [selectedPost, setSelectedPost] = React.useState<number | null>(null);
 
-  axiosReq
-    .get(accessTokenByTistory)
-    .then((response: any) => {
-      // 요청이 성공하면, response.data 또는 다른 방법으로 인증 코드를 처리합니다.
-      if (blogData !== null) return;
-      setBlogData(response.data.tistory.item.blogs[0]);
+  useEffect(() => {
+    const getTistoryData = () => {
       axiosReq
-        .get(getPostsByTistory + response.data.tistory.item.blogs[0].name)
-        .then((responseAboutPosts: any) => {
-          if (blogPosts !== null) return;
-          setBlogPosts(responseAboutPosts.data.tistory.item.posts);
-          responseAboutPosts.data.tistory.item.posts.map((post: any) => {
-            axiosReq
-              .get(
-                getPostById +
-                  response.data.tistory.item.blogs[0].name +
-                  "&postId=" +
-                  post.id
-              )
-              .then((element: any) => {
-                setBlogRecentlyPosts((prev) => [
-                  ...prev,
-                  element.data.tistory.item,
-                ]);
+        .get(accessTokenByTistory)
+        .then((response: any) => {
+          // 요청이 성공하면, response.data 또는 다른 방법으로 인증 코드를 처리합니다.
+          if (blogData !== null) return;
+          setBlogData(response.data.tistory.item.blogs[0]);
+          axiosReq
+            .get(getPostsByTistory + response.data.tistory.item.blogs[0].name)
+            .then((responseAboutPosts: any) => {
+              if (blogPosts !== null) return;
+              setBlogPosts(responseAboutPosts.data.tistory.item.posts);
+              responseAboutPosts.data.tistory.item.posts.map((post: any) => {
+                axiosReq
+                  .get(
+                    getPostById +
+                      response.data.tistory.item.blogs[0].name +
+                      "&postId=" +
+                      post.id
+                  )
+                  .then((element: any) => {
+                    setBlogRecentlyPosts((prev) => [
+                      ...prev,
+                      element.data.tistory.item,
+                    ]);
+                  });
               });
-          });
+            })
+            .catch((error: any) => {
+              // 요청이 실패한 경우 오류 처리
+              console.error("오류:", error);
+            });
         })
         .catch((error: any) => {
           // 요청이 실패한 경우 오류 처리
           console.error("오류:", error);
         });
-    })
-    .catch((error: any) => {
-      // 요청이 실패한 경우 오류 처리
-      console.error("오류:", error);
-    });
+    };
+    if (blogData === null) getTistoryData();
+  }, [blogData]);
 
   return (
     <S.BlogWrapper id="blog" ref={blogRef}>
       <S.BlogTitle>Blog</S.BlogTitle>
-      <S.BlogContent>
-        {blogData === null ? (
-          <div>Loading...</div>
-        ) : (
-          <>
-            <S.BlogThumbnail>
-              <S.BlogImage src={blogData.profileImageUrl} />
-              <S.BlogInfo>
-                <S.BlogName>{blogData.name}</S.BlogName>
-                <S.BlogDescription>{blogData.description}</S.BlogDescription>
-                <S.BlogStatistics>
-                  <S.BlogStatisticsItem>
-                    <strong>POSTS</strong>
-                    <p>{blogData.statistics.post}</p>
-                  </S.BlogStatisticsItem>
-                  <S.BlogStatisticsItem>
-                    <strong>COMMENTS</strong>
-                    <p>{blogData.statistics.comment}</p>
-                  </S.BlogStatisticsItem>
-                </S.BlogStatistics>
-                <S.BlogLink
-                  href={blogData.url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  블로그 바로가기
-                </S.BlogLink>
-              </S.BlogInfo>
-            </S.BlogThumbnail>
-            <S.BlogRecentlyPosts>
-              <S.BlogTitle>RECENTLY POSTS</S.BlogTitle>
-              {blogRecentlyPosts.length === 10 ? (
-                <RecentlyPosts
-                  innerHeight={innerHeight}
-                  innerWidth={innerWidth}
-                  posts={blogRecentlyPosts}
-                  setSelectedPost={setSelectedPost}
-                />
-              ) : (
-                <div
-                  style={{
-                    height: `${innerHeight * 0.65 * 0.6}px`,
-                    width: `${innerWidth * 0.8}px`,
-                  }}
-                />
-              )}
-            </S.BlogRecentlyPosts>
-          </>
-        )}
-      </S.BlogContent>
+      {blogData === null ? (
+        <div>Loading...</div>
+      ) : (
+        <S.BlogContent>
+          <S.BlogThumbnail>
+            <S.BlogImage src={blogData.profileImageUrl} />
+            <S.BlogInfo>
+              <S.BlogName>{blogData.name}</S.BlogName>
+              <S.BlogDescription>{blogData.description}</S.BlogDescription>
+              <S.BlogStatistics>
+                <S.BlogStatisticsItem>
+                  <strong>POSTS</strong>
+                  <p>{blogData.statistics.post}</p>
+                </S.BlogStatisticsItem>
+                <S.BlogStatisticsItem>
+                  <strong>COMMENTS</strong>
+                  <p>{blogData.statistics.comment}</p>
+                </S.BlogStatisticsItem>
+              </S.BlogStatistics>
+              <S.BlogLink href={blogData.url} target="_blank" rel="noreferrer">
+                블로그 바로가기
+              </S.BlogLink>
+            </S.BlogInfo>
+          </S.BlogThumbnail>
+          <S.BlogRecentlyPosts>
+            <S.BlogTitle>RECENTLY POSTS</S.BlogTitle>
+            {blogRecentlyPosts.length === 10 ? (
+              <RecentlyPosts
+                innerHeight={innerHeight}
+                innerWidth={innerWidth}
+                posts={blogRecentlyPosts.sort(
+                  (a, b) =>
+                    (new Date(b.date) as any) - (new Date(a.date) as any)
+                )}
+                setSelectedPost={setSelectedPost}
+              />
+            ) : (
+              <div
+                style={{
+                  height: `${innerHeight * 0.65 * 0.6}px`,
+                  width: `${innerWidth * 0.8}px`,
+                }}
+              />
+            )}
+          </S.BlogRecentlyPosts>
+        </S.BlogContent>
+      )}
       {selectedPost !== null && (
         <Modal
           open={typeof selectedPost === "number"}

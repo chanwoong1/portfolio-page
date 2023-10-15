a1e9e54a9fd67b4e70429330d2b5242b6257c5a1
chanwoong1 <jcw7468@gmail.com>
2023-10-15 18:27:15 +0900

    Chore: edit metadata

--- gatsby-site-meta-data.ts
+++ gatsby-site-meta-data.ts
@@ -5,6 +5,6 @@ export default {
     introduce1: "안녕하세요!",
     introduce2: "저는",
     introduce3: "를 좋아하는 개발자 정찬웅입니다.",
-    skills: ["frontend", "react", "typescript", "python", "data analysis"]
-  }
-}
+    skills: ["frontend", "react", "typescript", "python", "data science"],
+  },
+};
